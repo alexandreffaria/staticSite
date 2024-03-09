648ceda7800865fb8877fc375ca8c7dc06bb9eb2
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-03-09 01:42:21 -0300

    testing split_nodes_delimiter()

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -26,6 +26,49 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("Yoyo", "bold")
 
         self.assertEqual(node.text, node2.text)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_splitting_normal_text(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        result = split_nodes_delimiter([node], "`", text_type_code)
+        expected = [
+             TextNode("This is text with a ", "text"),
+             TextNode("code block", "code"),
+             TextNode(" word", "text"),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_splitting_bold_text(self):
+        node = TextNode("This is text with a **code block** word", text_type_text)
+        result = split_nodes_delimiter([node], "**", text_type_bold)
+        expected = [
+             TextNode("This is text with a ", "text"),
+             TextNode("code block", "bold"),
+             TextNode(" word", "text"),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_exception_on_missing_closing_delimiter(self):
+        node = TextNode("This is text with a `code block", "text")
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "`", text_type_code)
+
+    def test_multiple_node(self):
+            nodes = [TextNode("This is text with a `code block` word", "text"), TextNode("**bold** text", "text")]
+
+            results = split_nodes_delimiter(nodes, "`", text_type_code)
+            results = split_nodes_delimiter(results, "**", text_type_bold)
+
+            expected  = [
+                TextNode("This is text with a ", "text"),
+                TextNode("code block", "code"),
+                TextNode(" word", "text"),
+                TextNode("i bet ", "text"),
+                TextNode("bold", "bold"),
+                TextNode(" text", "text")
+            ]
+
+            self.assertEqual(results, expected)
 
 
 
