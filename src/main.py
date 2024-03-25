dc50072aa9b57ae3dd52db0fd3a0f524f7b2a8e4
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-03-25 17:32:09 -0300

    setting up copy recursive and generate index page

--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
 import os
 import shutil
+
 from copyStatic import copyRecursively
+from generate_page import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    print("Deleting public folder.")
+    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public dir...")
+    print("Copying static files to public directory...")
     copyRecursively(dir_path_static, dir_path_public)
 
-if __name__ == "__main__":
-    main()
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
+
+main()
